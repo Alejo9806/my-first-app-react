87684458c764ffd559557c1ae46c20f381a2bcfc
Alejo9806 <mello0616@gmail.com>
2021-08-12 11:32:30 -0500

    presentational and container components

--- src/components/DishDetailComponent.js
+++ src/components/DishDetailComponent.js
@@ -6,30 +6,19 @@ class DishDetail extends Component{
         super(props);
         this.state = {
         }
-        console.log('hola')
+
+    }
+    componentDidMount(){
+        console.log('DishDetail Component mount')
+    }
+    componentDidUpdate(){
+        console.log('DishDetail Component update')
     }
 
-    renderComments(dishComments) {
-        if (dishComments != null) {
+    renderDish(dishDetail){
+        if(dishDetail != null){
+            console.log(dishDetail)
             return(
-                dishComments.map((comment)=>{
-                    let date = comment.date.slice(0,10);
-                    return(
-                        <div key={comment.id}>
-                            <ListGroupItem className="border-0">{comment.comment}</ListGroupItem>
-                            <ListGroupItem className="border-0">-- {comment.author}, {date}</ListGroupItem>
-                        </div>
-                       
-                    )
-                })
-            )
-        }
-    }
-    render(){
-        const dishDetail = this.props.dishDetail;
-        console.log(dishDetail)
-        return(
-            <div className="container">
                 <div className="row">
                     <div className="col-12 col-sm-12 col-md-5 col-lg-5 m-1"> 
                         <Card>
@@ -46,6 +35,32 @@ class DishDetail extends Component{
                         
                     </div>
                 </div>
+            )
+        }
+    }
+    renderComments(dishComments) {
+        if (dishComments != null) {
+            return(
+                dishComments.map((comment)=>{
+                    let date = new Intl.DateTimeFormat('en-US',{year:'numeric',month:'short',day:'2-digit'}).format(new Date(Date.parse(comment.date)));
+                    return(
+                        <div key={comment.id}>
+                            <ListGroupItem className="border-0">{comment.comment}</ListGroupItem>
+                            <ListGroupItem className="border-0">-- {comment.author}, {date}</ListGroupItem>
+                        </div>
+                       
+                    )
+                })
+            )
+        }
+    }
+    
+    render(){
+        console.log('DishDetail Component render')
+        const dishDetail = this.props.dishDetail;
+        return(
+            <div className="container">
+                {this.renderDish(dishDetail)}
             </div>
         );
     }
