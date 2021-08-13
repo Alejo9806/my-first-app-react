1910e45a3dc7c9f92f61bc82ada679c6e86ebe91
Alejo9806 <mello0616@gmail.com>
2021-08-13 13:56:30 -0500

    react route

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { Component } from 'react';
 // import logo from './logo.svg';
 import './App.css';
 import Main from './components/MainComponent';
+import {BrowserRouter} from 'react-router-dom'
 
 
 
@@ -9,9 +10,11 @@ class App extends Component{
 
   render(){ 
     return (
+      <BrowserRouter>
       <div>
         <Main/>
       </div>
+      </BrowserRouter>
     );
   }
   
